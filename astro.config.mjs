13890f8a9d0decff591f8aa6db5d052243e27aec
kagan <intend.on.games+git@gmail.com>
2022-11-20 02:51:37 +0900

    astro.config.jsを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  trailingSlash: "always",
+  vite: {
+    server: {
+      open: true,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: "assets/[name].[hash].js",
+          entryFileNames: "assets/[name].[hash].js",
+        },
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer],
+      },
+      preprocessorOptions: {
+        scss: {
+          // TODO: importもしつつ、astroに直接書いてもエラーにならないようにしたい（そもそもastro側の問題？）
+          // additionalData: `
+          //   @use "${path.resolve(__dirname, "src")}/styles/global/global.scss" as *;
+          //   @use "sass:map";
+          // `,
+        },
+      },
+    },
+  },
+});
