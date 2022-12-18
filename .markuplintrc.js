4b96241a1ac263e487ecbd0c7b2d0d01174f78b4
kagan <intend.on.games+git@gmail.com>
2022-12-18 17:19:29 +0900

    コンフィグファイルに型付け

--- .markuplintrc.js
+++ .markuplintrc.js
@@ -1,19 +1,23 @@
-module.exports = {
-  extends: ["markuplint:recommended"],
-  // parser: {
-  //   ".astro$": "@markuplint/astro-parser",
-  // },
-  rules: {
-    "end-tag": true,
-    // 対応が難しいため
-    "require-accessible-name": false,
-  },
-  nodeRules: [
-    {
-      selector: ":where(video, audio)",
-      rules: {
-        "required-element": false, // "required-element": ["track"] を オフ
-      },
+// @ts-check
+
+module.exports =
+  /** @type {import('@markuplint/ml-config').Config} */
+  ({
+    extends: ["markuplint:recommended"],
+    // parser: {
+    //   ".astro$": "@markuplint/astro-parser",
+    // },
+    rules: {
+      "end-tag": true,
+      // 対応が難しいため
+      "require-accessible-name": false,
     },
-  ],
-};
+    nodeRules: [
+      {
+        selector: ":where(video, audio)",
+        rules: {
+          "required-element": false, // "required-element": ["track"] を オフ
+        },
+      },
+    ],
+  });
