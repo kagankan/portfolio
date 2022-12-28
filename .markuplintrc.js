72c680c884f2cef57cf38ab6e4b6b570e8b29e68
kagan <intend.on.games+git@gmail.com>
2022-12-29 03:00:31 +0900

    markuplintをアップデート

--- .markuplintrc.js
+++ .markuplintrc.js
@@ -3,21 +3,5 @@
 module.exports =
   /** @type {import('@markuplint/ml-config').Config} */
   ({
-    extends: ["markuplint:recommended"],
-    // parser: {
-    //   ".astro$": "@markuplint/astro-parser",
-    // },
-    rules: {
-      "end-tag": true,
-      // 対応が難しいため
-      "require-accessible-name": false,
-    },
-    nodeRules: [
-      {
-        selector: ":where(video, audio)",
-        rules: {
-          "required-element": false, // "required-element": ["track"] を オフ
-        },
-      },
-    ],
+    extends: ["markuplint:recommended-static-html"],
   });
